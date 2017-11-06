915e5e839d4914f640b13922824a284846e6d503
CARICOM12 <0190a@ukr.net>
2017-11-06 19:39:54 +0200

    It's a programm which calculates various currencies

---
+++ LearningCSharpBasicsRenovated/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        private decimal dollarToUa = 26.93m;
+        private decimal dollarToBy = 1.98m;
+        private decimal dollarToRu = 59.05m;
+        private decimal dollarToCn = 6.63m;
+        private decimal dollarToIn = 64.70m;
+        private decimal storage;
+
+        private decimal Input()
+        {
+            Console.WriteLine("Please type amount of money for a conversion: ");
+            storage = Convert.ToDecimal(Console.ReadLine());
+            return storage;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
